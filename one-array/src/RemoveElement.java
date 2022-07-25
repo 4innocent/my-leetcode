5991e75180a6d588c75bff1151ef92702945ac00
龚康佳 <gongkj@eastcom-sw.com>
2022-07-25 09:06:48 +0800

    移除元素

--- one-array/src/RemoveElement.java
+++ one-array/src/RemoveElement.java
@@ -1,6 +1,6 @@
 public class RemoveElement {
     public static void main(String[] args) {
-        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int[] nums = {0,1,2,2,3,0,4,2};
         int val = 2;
         System.out.println(solution(nums, val));
         for (int num : nums) {
@@ -9,23 +9,14 @@ public class RemoveElement {
     }
 
     public static int solution(int[] nums, int val) {
-        if (nums.length == 0) return -1;
+        if (nums.length == 0) return 0;
         int slowIndex = 0;
         int fastIndex = 0;
-        int indent = 0;
         while (fastIndex <= nums.length - 1) {
-            if (nums[fastIndex] != val) {
-                fastIndex++;
-                slowIndex++;
-            } else {
-                while (fastIndex < nums.length && nums[fastIndex] == val) {
-                    indent++;
-                    fastIndex++;
-                }
-                if (fastIndex < nums.length) {
-                    nums[fastIndex - indent + 1] = nums[fastIndex];
-                }
+            if(nums[fastIndex] != val){
+                nums[slowIndex++] = nums[fastIndex];
             }
+            fastIndex++;
         }
         return slowIndex;
     }
